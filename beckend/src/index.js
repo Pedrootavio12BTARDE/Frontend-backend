345419eb1d0dbba698d909ad7b9a898b13450d39
Pedro Otavio <pedromartins06072008@gmail.com>
2025-11-06 22:20:46 -0300

    Envio da nova lição do Marcio Marçal

--- beckend/src/index.js
+++ beckend/src/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express"
+import express, { request, response } from "express"
 import {people} from "./people.js"
 import cors from "cors"
 
@@ -7,10 +7,29 @@ const app = express()
 const port = 3333
 
 app.use(cors())
- 
+app.use(express.json())
+
 app.get("/", (request, response) => {
     response.json(people)
 })
+
+
+app.post("/cadastrar",(request,response ) =>{
+    const {name,email,age,password} = request.body.user
+
+
+
+    console.log (`
+       Nome : ${name},
+       E-mail : ${email},
+       Idade :${age},
+       Senha : ${password}
+
+        `)
+
+        response.status(201).json({message:"Usuário cadastrado com sucesso!"})
+
+ })
 
 
 app.listen(port, () =>{
